f0234d0c5719c8ae4c7f7ee59d1b2f874f100c53
Iván portillo <ipp_1981@hotmail.com>
2022-06-18 21:59:44 +0200

    creada clase jueguete

---
+++ src/Liskov/Juguete.php
@@ -0,0 +1,19 @@
+
+<?php 
+// El objetivo de Liskov es que una Clase H extendida de P, entonces los objetos de P pueden ser sustituidos por objetos del tipo H.
+
+
+// El objetivo del juguete es insertar figuras geometricas en los orificios que contienealos lados.
+// Al tratar de insertar una figura en un orificio tenemos dos posibles resultados:
+// 1. entro:verdadero
+// 2. no entro:falso
+// Para saber si una figura entra en el orificio,requerimos saber dos cosas:
+// 1. El area de la fogira que queremos insertar
+// 2. El area del orificio donde queremos insertar la figura
+
+namespace App\Liskov;
+
+class Jueguete
+{
+
+}
